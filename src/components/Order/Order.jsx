50f7181b8ec27e3712b38f11266387f0e34ea40a
Moisés Xavier <xavier.moises8@gmail.com>
2023-09-07 02:42:18 -0300

    feat: função de ordenar os items pelos valores

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -6,42 +6,29 @@ import {
 } from 'react-icons/ai';
 
 const Order = ({ sequence, setSequence }) => {
-  console.log(sequence);
-
-  switch (sequence) {
-    case 'relevant':
-  }
-
+  const buttonStyles = `text-lg font-semibold border-2 border-gray-500 rounded-lg bg-gray-300 disabled:bg-gray-500 hover:bg-gray-400 px-3 flex items-center justify-center gap-2`;
   return (
     <section className="flex justify-end gap-2">
-      {sequence === 'relevant' ? (
-        <CustomButton
-          className={`text-lg font-semibold border-2 border-gray-500 rounded-lg bg-gray-400 px-3 flex items-center justify-center gap-2`}
-          disabled
-          onClick={() => setSequence('relevant')}
-        >
-          Relevante
-          <AiFillStar />
-        </CustomButton>
-      ) : (
-        <CustomButton
-          className={`text-lg font-semibold border-2 border-gray-500 rounded-lg bg-gray-300 hover:bg-gray-400 px-3 flex items-center justify-center gap-2`}
-          onClick={() => setSequence('relevant')}
-        >
-          Relevante
-          <AiFillStar />
-        </CustomButton>
-      )}
       <CustomButton
-        className="text-lg font-semibold border-2 border-gray-500 rounded-lg bg-gray-300 hover:bg-gray-400 px-3 flex items-center justify-center gap-2"
+        className={buttonStyles}
+        onClick={() => setSequence('relevant')}
+        disabled={sequence === 'relevant' ? 'relevant' : ''}
+      >
+        Relevante
+        <AiFillStar />
+      </CustomButton>
+      <CustomButton
+        className={buttonStyles}
         onClick={() => setSequence('asc')}
+        disabled={sequence === 'asc' ? 'relevant' : ''}
       >
         Crescente
         <AiOutlineArrowUp />
       </CustomButton>
       <CustomButton
-        className="text-lg font-semibold border-2  border-gray-500 rounded-lg bg-gray-300 hover:bg-gray-400 px-3 flex items-center justify-center gap-2"
+        className={buttonStyles}
         onClick={() => setSequence('desc')}
+        disabled={sequence === 'desc' ? 'relevant' : ''}
       >
         Decrescente
         <AiOutlineArrowDown />
