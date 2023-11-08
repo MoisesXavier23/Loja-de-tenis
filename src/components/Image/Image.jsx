c2e40b46d1c4246febd1099c901cdbbe85ab24a0
Moisés Xavier <xavier.moises8@gmail.com>
2023-11-08 13:32:38 -0300

    Pasta assets movida para public

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -2,7 +2,7 @@ const ImageSlider = () => {
   return (
     <div
       style={{
-        backgroundImage: `url(src/assets/Promo-Image.png)`,
+        backgroundImage: `url(public/assets/Promo-Image.png)`,
       }}
       className="max-w-full w-full min-[320px]:h-[160px] min-[425px]:h-[180px] min-[500px]:h-[280px] max-[540px]:h-[220px] min-[768px]:h-[320px] min-[1024px]:h-[430px] xl:h-[450px] bg-cover bg-center bg-no-repeat"
     ></div>
